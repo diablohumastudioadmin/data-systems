cc54731c1cf4a78636b9b0761b28293832d7f326
diablohumastudioadmin <admin@diablohumastudio.com>
2026-01-28 09:53:05 -0500

    commented everything in dbmanager so start from scratch

--- addons/diablo_huma_studio/db_manager/db_manager.gd
+++ addons/diablo_huma_studio/db_manager/db_manager.gd
@@ -8,10 +8,6 @@ const DATA_FOLDER_NAME: String = "data/"
 
 func _ready() -> void:
 	create_data_types_folder()
-	EditorInterface.get_resource_filesystem().filesystem_changed.connect(_on_editor_fs_fs_changed)
-
-func _on_editor_fs_fs_changed():
-	print("fs changed")
 
 func create_data_types_folder():
 	if not DirAccess.dir_exists_absolute(BRAND_PATH + PLUGIN_FOLDER_NAME + DATA_FOLDER_NAME):
@@ -35,44 +31,44 @@ func create_tables():
 func create_table():
 	pass
 
-func create_level_db():
-	return
-	var new_level_db: LevelDB = LevelDB.new()
-	new_level_db.levels = get_all_level_data_files()
-	ResourceSaver.save(new_level_db, "res://data_system/level/levelDB/levelDB.tres")
-	ResourceLoader.load("res://data_system/level/levelDB/levelDB.tres","",ResourceLoader.CACHE_MODE_REPLACE)
-	if OS.has_feature("standalone"): EditorInterface.get_resource_filesystem().scan()
-	print("levelDB created and saved in res://data_system/level/levelDB/levelDB.tres")
-
-func create_achivements_db():
-	return
-	var new_ahievement_db: AchievementDB = AchievementDB.new()
-	new_ahievement_db.achievements = get_all_achievement_data_files()
-	ResourceSaver.save(new_ahievement_db, "res://data_system/achievement/achievementDB/achievementDB.tres")
-	ResourceLoader.load("res://data_system/achievement/achievementDB/achievementDB.tres","",ResourceLoader.CACHE_MODE_REPLACE)
-	if OS.has_feature("standalone"): EditorInterface.get_resource_filesystem().scan()
-	print("levelDB created and saved in res://data_system/achievement/achievementDB/achievementDB.tres")
-
-func get_all_level_data_files() -> Array[LevelData]:
-	var level_array: Array[LevelData]
-	var data_folder_path: String = "res://data_system/level/data/"
-	var dir = DirAccess.open(data_folder_path)
-	var files: PackedStringArray
-	if dir: files = dir.get_files()
-	for file in files:
-		var level_ = load(data_folder_path + file)
-		if level_ is LevelData:
-			level_array.append(level_)
-	return level_array
-
-func get_all_achievement_data_files() -> Array[AchievementData]:
-	var achievement_array: Array[AchievementData]
-	var data_folder_path: String = "res://data_system/achievement/data/"
-	var dir = DirAccess.open(data_folder_path)
-	var files: PackedStringArray
-	if dir: files = dir.get_files()
-	for file in files:
-		var achievement_ = load(data_folder_path + file)
-		if achievement_ is AchievementData:
-			achievement_array.append(achievement_)
-	return achievement_array
+#func create_level_db():
+	#return
+	#var new_level_db: LevelDB = LevelDB.new()
+	#new_level_db.levels = get_all_level_data_files()
+	#ResourceSaver.save(new_level_db, "res://data_system/level/levelDB/levelDB.tres")
+	#ResourceLoader.load("res://data_system/level/levelDB/levelDB.tres","",ResourceLoader.CACHE_MODE_REPLACE)
+	#if OS.has_feature("standalone"): EditorInterface.get_resource_filesystem().scan()
+	#print("levelDB created and saved in res://data_system/level/levelDB/levelDB.tres")
+#
+#func create_achivements_db():
+	#return
+	#var new_ahievement_db: AchievementDB = AchievementDB.new()
+	#new_ahievement_db.achievements = get_all_achievement_data_files()
+	#ResourceSaver.save(new_ahievement_db, "res://data_system/achievement/achievementDB/achievementDB.tres")
+	#ResourceLoader.load("res://data_system/achievement/achievementDB/achievementDB.tres","",ResourceLoader.CACHE_MODE_REPLACE)
+	#if OS.has_feature("standalone"): EditorInterface.get_resource_filesystem().scan()
+	#print("levelDB created and saved in res://data_system/achievement/achievementDB/achievementDB.tres")
+#
+#func get_all_level_data_files() -> Array[LevelData]:
+	#var level_array: Array[LevelData]
+	#var data_folder_path: String = "res://data_system/level/data/"
+	#var dir = DirAccess.open(data_folder_path)
+	#var files: PackedStringArray
+	#if dir: files = dir.get_files()
+	#for file in files:
+		#var level_ = load(data_folder_path + file)
+		#if level_ is LevelData:
+			#level_array.append(level_)
+	#return level_array
+#
+#func get_all_achievement_data_files() -> Array[AchievementData]:
+	#var achievement_array: Array[AchievementData]
+	#var data_folder_path: String = "res://data_system/achievement/data/"
+	#var dir = DirAccess.open(data_folder_path)
+	#var files: PackedStringArray
+	#if dir: files = dir.get_files()
+	#for file in files:
+		#var achievement_ = load(data_folder_path + file)
+		#if achievement_ is AchievementData:
+			#achievement_array.append(achievement_)
+	#return achievement_array
